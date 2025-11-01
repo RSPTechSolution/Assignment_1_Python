d4e4d53a0dc9712b7ed56ea3d8dd950ad107e048
RSPInfoTech <89846204+RSPTechSolution@users.noreply.github.com>
2025-11-01 10:49:10 +0530

    First Commit

---
+++ concate.py
@@ -0,0 +1,6 @@
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+
+greeting = "Hello, " + firstName + " " + lastName + "! " + "Welcome to the Python program."
+
+print(greeting)
